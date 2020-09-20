46290a3a466a1b61c6b8ed24bb5baa1751c79245
918101630110 <55906113+918101630110@users.noreply.github.com>
2020-09-20 20:53:34 +0800

    QT更新（基本完成）（只包含项目文件）

---
+++ llk_game/option.h
@@ -0,0 +1,39 @@
+#ifndef OPTION_H
+#define OPTION_H
+
+#include<QDialog>
+#include<QRadioButton>
+#include<QGroupBox>
+#include<QPushButton>
+class Option:public QDialog
+{
+    Q_OBJECT
+private:
+    int levels;
+    int types;
+
+    QGroupBox* level_box;
+    QRadioButton* easy_button;
+    QRadioButton* mid_button;
+    QRadioButton* hard_button;
+
+    QGroupBox* type_box;
+    QRadioButton* animal_button;
+    QRadioButton* life_button;
+    QRadioButton* dota_button;
+
+    QPushButton* ok_button;
+
+public:
+    Option(QWidget* parent = 0);
+    int getlevels();
+
+public slots:
+    void changeOption();
+
+signals:
+    void newOption(int,int);
+
+};
+
+#endif // OPTION_H
